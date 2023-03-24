48474802379f8379a48fffe82972be1b1c871cd7
joan lovelle alfeche <jlovellealfeche@gmail.com>
2023-03-24 22:17:31 +0100

    added component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useReducer} from "react";
 import DigitButton from "./DigitButton";
+import OperationButton from "./OperationButton";
 import './App.css';
 
 export const ACTIONS = {
@@ -17,7 +18,7 @@ function reducer(state, {type, payload}) {
     case ACTIONS.ADD_DIGIT:
       return {
         ...state,
-        currentOperand: `${currentOperand || ""}  ${payload.digit}`,
+        currentOperand: `${state.currentOperand || ""}  ${payload.digit}`,
     }
   }
 }
@@ -43,24 +44,19 @@ function App() {
           {/* Add the resetInput or AC button */} 
         <button >DEL</button>
         {/* Add the resetResult or Del button */}
-        <OperationButton digit="÷" dispatch={dispatch} />
-          {/* Add the divide button */} 
-      
-
+        <OperationButton operation="÷" dispatch={dispatch} />
         <DigitButton digit="1" dispatch={dispatch} />
         <DigitButton digit="2" dispatch={dispatch} />
         <DigitButton digit="3" dispatch={dispatch} />
-        <OperationButton digit="*" dispatch={dispatch} />
-          {/* Add the multiply button */} 
+        <OperationButton operation="*" dispatch={dispatch} />      
         <DigitButton digit="4" dispatch={dispatch} />
         <DigitButton digit="5" dispatch={dispatch} />
         <DigitButton digit="6" dispatch={dispatch} />
-        <OperationButton digit="+" dispatch={dispatch} />
+        <OperationButton operation="+" dispatch={dispatch} />
         <DigitButton digit="7" dispatch={dispatch} />
         <DigitButton digit="8" dispatch={dispatch} />
         <DigitButton digit="9" dispatch={dispatch} />
-        <OperationButton digit="-" dispatch={dispatch} />
-          {/* Add the subtract button */}
+        <OperationButton operation="-" dispatch={dispatch} />
         <DigitButton digit="." dispatch={dispatch} />
         <DigitButton digit="0" dispatch={dispatch} />
         <button className="span-two"> = </button> {/* add thefunction the current total */}
