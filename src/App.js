5bd0ea5126903c29ac00cffb92fbc04749a33af5
joan lovelle alfeche <jlovellealfeche@gmail.com>
2023-03-24 21:20:29 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,108 +1,62 @@
 import {
-  useState,
-  useRef
+  useReducer
 } from "react";
 import './App.css';
 
+const ACTIONS = {
+  ADD_DIGIT: 'add-digit',
+  CHOOSE_OPERATION:'choose-operation',
+  CLEAR: 'clear',
+  DELETE_DIGIT: 'delete-digit',
+  EVALUATE: 'evaluate'
+
+}
+
+
+function reducer(state, {type, payload}) {
+
+}
+
 function App() {
+  const [{currentOperand, previousOperand, operation}, dispatch] = useReducer(reducer, 
+    {});
+ 
+ 
 
- const inputRef = useRef(null); 
-  const resultRef = useRef(null); 
-  const [result, setResult] = useState(0); 
- 
-  function plus(e) { 
-    e.preventDefault(); 
-    // const inputVal = inputRef.current.value; 
-    // const newResult = result + Number(inputVal); 
-    // setResult(newResult);
-    setResult((result) => result + Number(inputRef.current.value)); 
-  }; 
- 
-  function minus(e) { 
-    e.preventDefault();
-    const inputVal = inputRef.current.value;
-    const newResult = result - Number(inputVal)
-    setResult(newResult);
-  	// Add the code for the minus function 
-  };
- 
-  function times(e) { 
-    e.preventDefault();
-    const inputVal = inputRef.current.value;
-    const newResult = result *  Number(inputVal)
-    setResult(newResult);
-    // Add the code for the plus function 
-  }; 
- 
-  function divide(e) { 
-    e.preventDefault();
-    const inputVal = inputRef.current.value;
-    const newResult = result / Number(inputVal)
-    setResult(newResult);
-    // Add the code for the divide function 
-  };
- 
-  function resetInput(e) { 
-    // Add the code for the resetInput function 
-    e.preventDefault();
-    inputRef.current.value = 0;
-  }; 
- 
-  function resetResult(e) { 
-    e.preventDefault();
-    setResult("");
-  	// Add the code for the resetResult function 
-  }; 
-
-  // function total(e) {
-  //   e.preventDefault();
-  //   setResult(newResult);
-  // this is the function for the = sign }
- 
   return ( 
     <div className="App"> 
       <div className="calculator-grid">
       
          <br></br>
         <div className="output">
-           <div className="previous-operand" ref={resultRef}>123,456</div>
-           <div className="current-operand">{result} 123,456</div>
+           <div className="previous-operand">{previousOperand} {operation}</div>
+           <div className="current-operand">{currentOperand} </div>
         </div>
-        <button onClick={resetInput} className="span-two">AC</button>
+        <button className="span-two">AC</button>
           {/* Add the resetInput or AC button */} 
-        <button onClick={resetResult}>DEL</button>
+        <button >DEL</button>
         {/* Add the resetResult or Del button */}
-        <button onClick={divide}> ÷ </button>
+        <button > ÷ </button>
           {/* Add the divide button */} 
         <button>1</button>
         <button>2</button>
         <button>3</button>
-        <button onClick={times}>*</button>
+        <button >*</button>
           {/* Add the multiply button */} 
         <button>4</button>
         <button>5</button>
         <button>6</button>
-        <button onClick={plus}>+</button> 
+        <button >+</button> 
         <button>7</button>
         <button>8</button>
         <button>9</button>
-        <button onClick={minus}>-</button>
+        <button >-</button>
           {/* Add the subtract button */}
         <button>.</button>
         <button>0</button>
         <button className="span-two"> = </button> {/* add thefunction the current total */}
 
        
-
-
-        <form> 
-          <p ref={resultRef}>
-            {result}  {/* add the value of the current total */} 
-          </p> 
-          
-          
-          <span ref={resultRef}>{result}</span>
-        </form> 
       </div> 
     </div>
   ); 
