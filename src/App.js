95f03adb345584803d9c1eb8cc317cc7cf94771d
joan lovelle alfeche <jlovellealfeche@gmail.com>
2023-03-25 11:10:44 +0100

    finalizing del buttons onclick and fnx

--- src/App.js
+++ src/App.js
@@ -16,6 +16,14 @@ export const ACTIONS = {
 function reducer(state, {type, payload}) {
   switch(type) {
     case ACTIONS.ADD_DIGIT:
+      if (state.overwrite) {
+          return {
+            ...state,
+            currentOperand: payload.digit,
+            overwrite: false,
+          }
+      }
+
       if (payload.digit === "0" && state.currentOperand === "0") {
         return state
       } 
@@ -25,7 +33,7 @@ function reducer(state, {type, payload}) {
 
       return {
         ...state,
-        currentOperand: `${state.currentOperand || "" }  ${payload.digit}`,
+        currentOperand: `${state.currentOperand || "" }${payload.digit}`,
     }
     case ACTIONS.CHOOSE_OPERATION:
         if (state.currentOperand == null && state.previousOperand == null) {
@@ -56,6 +64,32 @@ function reducer(state, {type, payload}) {
         }
     case ACTIONS.CLEAR:
         return {} 
+
+    case ACTIONS.DELETE_DIGIT: 
+        if (state.overwrite) {
+          return {
+            ...state,
+            overwrite: false,
+            currentOperand: null
+          }
+        }
+
+        if (state.currentOperand == null) {
+          return state
+        }
+
+        if (state.currentOperand.length === 1) {
+          return {
+            ...state,
+            currentOperand: null,
+          }
+        }
+
+        return {
+          ...state,
+          currentOperand: state.currentOperand.slice(0, -1 ),
+        }
+
     case ACTIONS.EVALUATE: 
         if ( state.operation == null || state.currentOperand == null ||  state.previousOperand
           == null) {
@@ -64,6 +98,7 @@ function reducer(state, {type, payload}) {
 
           return {
             ...state,
+            overwrite: true,
             previousOperand: null,
             opereation: null,
             currentOperand: evaluate(state),
@@ -112,10 +147,15 @@ function App() {
            <div className="previous-operand">{previousOperand} {operation}</div>
            <div className="current-operand">{currentOperand} </div>
         </div>
-        <button className="span-two" onClick={() => dispatch({ type: ACTIONS.CLEAR })}>AC</button>
-          {/* Add the resetInput or AC button */} 
-        <button >DEL</button>
-        {/* Add the resetResult or Del button */}
+        <button className="span-two"
+         onClick={() => dispatch({ type: ACTIONS.CLEAR })}>
+          AC
+          </button>
+      
+        <button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>
+          DEL
+          </button>
+        
         <OperationButton operation="÷" dispatch={dispatch} />
         <DigitButton digit="1" dispatch={dispatch} />
         <DigitButton digit="2" dispatch={dispatch} />
