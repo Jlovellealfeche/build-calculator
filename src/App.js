14647ef3c9a3ac02ba553f336e443df7b8877266
joan lovelle alfeche <jlovellealfeche@gmail.com>
2023-03-25 10:15:07 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,11 +16,48 @@ export const ACTIONS = {
 function reducer(state, {type, payload}) {
   switch(type) {
     case ACTIONS.ADD_DIGIT:
-      if(payload.digit === "0" && state.currentOperand === "0") return state
+      if (payload.digit === "0" && state.currentOperand === "0") {
+        return state
+      } 
+      if (payload.digit === "." && state.currentOperand.includes(".")) {
+        return state
+      }
+
       return {
         ...state,
-        currentOperand: `${state.currentOperand || ""}  ${payload.digit}`,
+        currentOperand: `${state.currentOperand || "" }  ${payload.digit}`,
     }
+    case ACTIONS.CHOOSE_OPERATION:
+        if (state.currentOperand == null && state.previousOperand == null) {
+          return state
+        }
+        if (state.previousOperand == null) {
+          return {
+            ...state,
+            operation: payload.operation,
+            previousOperand: state.currentOperand,
+            currentOperand: null,
+          }
+        }
+
+        return {
+          ...state,
+          previousOperand: evaluate(state),
+          operation: payload.operation,
+          currentOperand: null,
+        }
+    case ACTIONS.CLEAR:
+        return {} 
+  }
+}
+
+function evaluate({currentOperand, previousOperand, operation}) {
+  const prev = parseFloat(previousOperand)
+  const current = parseFloat(currentOperand)
+  if (isNaN(prev) || isNaN (current)) return ""
+  let computation = ""
+  switch (operation) {
+    
   }
 }
 
@@ -41,7 +78,7 @@ function App() {
            <div className="previous-operand">{previousOperand} {operation}</div>
            <div className="current-operand">{currentOperand} </div>
         </div>
-        <button className="span-two">AC</button>
+        <button className="span-two" onClick={() => dispatch({ type: ACTIONS.CLEAR })}>AC</button>
           {/* Add the resetInput or AC button */} 
         <button >DEL</button>
         {/* Add the resetResult or Del button */}
